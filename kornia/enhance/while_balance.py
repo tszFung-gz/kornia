3cbc4a88f5b894b35ad441c336da04db2d6ec1db
tszfeng <tszfeng@stu.scau.edu.cn>
2022-04-04 17:58:35 +0800

    The method of white balance are classified and initially coded

--- kornia/enhance/while_balance.py
+++ kornia/enhance/while_balance.py
@@ -1,26 +1,43 @@
 """In this module several equalization methods are exposed: he, ahe, clahe."""
 
-import math
 from typing import Tuple
 
 import torch
 import torch.nn.functional as F
+from torchvision import transforms
 
 from kornia.utils.helpers import _torch_histc_cast
 from kornia.utils.image import perform_keep_shape_image
 from kornia.color import rgb_to_lab, lab_to_rgb
+from PIL import Image
 
-from .histogram import histogram
+
+def gray_world_assumption(imgs: torch.Tensor) -> torch.Tensor:
+    result = rgb_to_lab(imgs)
+    avg_a = torch.mean(result[:, 1, :, :])
+    avg_b = torch.mean(result[:, 2, :, :])
+    result[:, 1, :, :] = result[:, 1, :, :] - (avg_a * (result[:, 1, :, :] / 100.0) * 1.1)
+    result[:, 2, :, :] = result[:, 2, :, :] - (avg_b * (result[:, 2, :, :] / 100.0) * 1.1)
+    result[:, :, :, :] = lab_to_rgb(result[:, :, :, :])
+    return result
+
+def perfect_reflector_assumption(imgs: torch.Tensor) -> torch.Tensor:
+    r, g, b = torch.split(imgs, split_size_or_sections=1, dim=1)
+    MaxVal = torch.max(torch.max(torch.maximum(torch.maximum(r, g), b), dim=2)[0], dim=2)[0]
+    ratio = 0.05
+    ratio = imgs.shape[2] * imgs.shape[3] * ratio
 
 
-def white_balance(imgs: torch.Tensor, grid_size: Tuple[int, int],
-                 even_tile_size: bool = False ) -> torch.Tensor:
+def dynamic_threshold(imgs: torch.Tensor):
+    print(imgs.shape)
+
+
+
+def white_balance(imgs: torch.Tensor, gray_world = False, perfect_reflect = False) -> torch.Tensor:
     """_summary_
 
     Args:
         imgs (torch.Tensor): _description_
-        grid_size (Tuple[int, int]): _description_
-        even_tile_size (bool, optional): _description_. Defaults to False.
 
     Raises:
         TypeError: _description_
@@ -30,15 +47,25 @@ def white_balance(imgs: torch.Tensor, grid_size: Tuple[int, int],
         torch.Tensor: _description_
     """
     if not isinstance(imgs, torch.Tensor):
-        raise TypeError(f"Input type is not a torch.Tensor. Got {type(imgs)}")      
-    if len(imgs.shape) < 3 or imgs.shape[-3] != 3:         
+        raise TypeError(f"Input type is not a torch.Tensor. Got {type(imgs)}")
+    if len(imgs.shape) < 3 or imgs.shape[-3] != 3:
         raise ValueError(f"Input size must have a shape of (*, 3, H, W). Got {imgs.shape}")
-    result = rgb_to_lab(imgs)
-    for i in range(result.shape[0]):
-        avg_a = torch.mean(result[i, :, :, 1])
-        avg_b = torch.mean(result[i, :, :, 2])
-        result[i, :, :, 1] = result[i, :, :, 1] - ((avg_a - 128) * (result[i,:, :, 0] / 255.0) * 1.1)
-        result[i,:, :, 2] = result[i,:, :, 2] - ((avg_b - 128) * (result[i,:, :, 0] / 255.0) * 1.1)
-        result[i,:, :, :] = lab_to_rgb(result[i,:, :, :])
+    if gray_world:
+        result = gray_world_assumption(imgs)
+    elif perfect_reflect:
+        result = perfect_reflector_assumption(imgs)
+
     return result
 
+
+if __name__ == '__main__':
+    # img = Image.open().convert('RGB')
+    # # img.show()
+    # tf1 = transforms.Compose([transforms.ToTensor()])
+    # toImg = transforms.ToPILImage()
+    # img = tf1(img).unsqueeze(0)
+    img = torch.rand((16,3,256,256))
+    output = white_balance(img, perfect_reflect=True)
+    output = toImg(output[0])
+    output.show()
+    # print(output.shape)
